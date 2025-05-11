7acd756331a7718d34ff1a1e8b6b25fe42416bab
Crvsps <129600035+Crvsps@users.noreply.github.com>
2025-05-11 15:57:32 +0200

    maze view (proto)

--- CYnapse/cynapse/src/main/java/projatlab/view/GenerationView.java
+++ CYnapse/cynapse/src/main/java/projatlab/view/GenerationView.java
@@ -124,7 +124,10 @@ public class GenerationView {
         // Button Generate
         Button btnGenerate = new Button("Générer un labyrinthe");
         btnGenerate.setPrefSize(150, 40);
-        btnGenerate.setOnAction(e -> openResWindow());
+        btnGenerate.setOnAction(e -> {
+        MazeView mazeView = new MazeView();
+        mazeView.show();
+    });
 
         VBox vbBotRight = new VBox();
         vbBotRight.getChildren().addAll(btnLoad, btnGenerate);
--- CYnapse/cynapse/src/main/java/projatlab/view/MazeView.java
+++ CYnapse/cynapse/src/main/java/projatlab/view/MazeView.java
@@ -1,5 +1,40 @@
 package projatlab.view;
 
+import javafx.scene.Scene;
+import javafx.scene.layout.GridPane;
+import javafx.scene.paint.Color;
+import javafx.scene.shape.Rectangle;
+import javafx.stage.Stage;
+
 public class MazeView {
-    
+
+    public void show() {
+        Stage stage = new Stage();
+        stage.setTitle("Labyrinthe généré");
+        stage.setResizable(false);
+
+        int rows = 10;
+        int cols = 10;
+        int cellSize = 25;
+
+        GridPane grid = new GridPane();
+
+        for (int y = 0; y < rows; y++) {
+            for (int x = 0; x < cols; x++) {
+                Rectangle rect = new Rectangle(cellSize, cellSize);
+                rect.setFill(Color.WHITE);
+                rect.setStroke(Color.BLACK); 
+                rect.setStrokeWidth(1);
+
+                grid.add(rect, x, y);
+            }
+        }
+
+        int totalWidth = cols * (cellSize + 1) ;
+        int totalHeight = rows * (cellSize + 1);
+
+        Scene scene = new Scene(grid, totalWidth, totalHeight);
+        stage.setScene(scene);
+        stage.show();   
+    }
 }
