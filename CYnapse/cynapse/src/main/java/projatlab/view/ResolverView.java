578295b289593840dc8c088241ac5350564c22d4
Crvsps <129600035+Crvsps@users.noreply.github.com>
2025-05-12 16:55:11 +0200

    Interface Résolution

--- CYnapse/cynapse/src/main/java/projatlab/view/ResolverView.java
+++ CYnapse/cynapse/src/main/java/projatlab/view/ResolverView.java
@@ -1,20 +1,100 @@
 package projatlab.view;
 
+import javafx.geometry.Pos;
 import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.CheckBox;
+import javafx.scene.control.Label;
+import javafx.scene.control.Separator;
+import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.Priority;
 import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
 
 public class ResolverView {
     public void show() {
         Stage resStage = new Stage();
-        VBox root = new VBox();
+        BorderPane root = new BorderPane();
 
-        // Ajout du labyrinthe
+        //Center
         MazeView mazeView = new MazeView();
-        root.getChildren().add(mazeView.getMazeNode());
+        root.setCenter(mazeView.getMazeNode());
+
+        //Right
+        VBox vbAlgoMode = new VBox();
+        vbAlgoMode.setAlignment(Pos.TOP_LEFT);
+
+        Label lAlgo = new Label("Choix de l'algorithme :");
+        CheckBox cb1 = new CheckBox();
+        CheckBox cb2 = new CheckBox();
+        CheckBox cb3 = new CheckBox();
+
+        CheckBox[] algoBoxes = {cb1, cb2, cb3};
+
+        for (CheckBox cb : algoBoxes) {
+            cb.setOnAction(e -> {
+                if (cb.isSelected()) {
+                    for (CheckBox other : algoBoxes) {
+                        if (other != cb) {
+                            other.setSelected(false);
+                        }
+                    }
+                }
+            });
+        }
+
+
+        Label lMode = new Label("Mode :");
+        CheckBox cbComplet = new CheckBox("Complet");
+        CheckBox cbPasComplet = new CheckBox("Pas Complet");
+
+        cbComplet.setOnAction(e -> {
+            if (cbComplet.isSelected()) {
+                cbPasComplet.setSelected(false);
+            }
+        });
+
+        cbPasComplet.setOnAction(e -> {
+            if (cbPasComplet.isSelected()) {
+                cbComplet.setSelected(false);
+            }
+        });
+
+        vbAlgoMode.getChildren().addAll(lAlgo, cb1, cb2, cb3, new Separator(), lMode, cbComplet, cbPasComplet);
+        VBox.setVgrow(vbAlgoMode, Priority.ALWAYS);
+        root.setRight(vbAlgoMode);
+
+        //Bottom
+
+        VBox vbStatsSave = new VBox();
+
+        VBox vbStats = new VBox();
+        vbStats.setAlignment(Pos.CENTER_LEFT);
+        Label lVisited = new Label("Cases visitées :");
+        Label lTime = new Label("Temps de résolution :");
+
+        vbStats.getChildren().addAll(lVisited, lTime);
+
+        Button bSave = new Button("Sauvegarder");
+        Button bSolve = new Button("Résoudre");
+        Button bModify = new Button("Modifier");
+
+        HBox hbSaveModSolve = new HBox(bSave,bModify,bSolve);
+
+        hbSaveModSolve.setMaxWidth(Double.MAX_VALUE);
+        VBox.setVgrow(hbSaveModSolve, Priority.NEVER);
+        hbSaveModSolve.setAlignment(Pos.BOTTOM_RIGHT);
+
+        
+        vbStatsSave.getChildren().addAll(vbStats, hbSaveModSolve);
+        root.setBottom(vbStatsSave);
 
         Scene scene = new Scene(root);
+
+
         resStage.setScene(scene);
+        resStage.setResizable(false);
         resStage.setTitle("Résolution du labyrinthe");
         resStage.show();
     }
