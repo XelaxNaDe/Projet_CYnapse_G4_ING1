782b36ea5b360923f0dbae6b949107051fae671e
XelaxNaDe <olivierbriard@outlook.fr>
2025-05-12 21:15:59 +0200

    Update MazeView.java

--- CYnapse/cynapse/src/main/java/projatlab/view/MazeView.java
+++ CYnapse/cynapse/src/main/java/projatlab/view/MazeView.java
@@ -20,69 +20,69 @@ public class MazeView extends Pane {
     }
 
     public void drawMaze() {
-    getChildren().clear();
+        getChildren().clear();
 
-    // 1. Fond des cellules visités
-    for (Cell[] row : maze.getGrid()) {
-        for (Cell cell : row) {
-            if (cell.visited) {
-                Rectangle background = new Rectangle(
-                    cell.j * cellSize,
-                    cell.i * cellSize,
-                    cellSize,
-                    cellSize
-                );
-                background.setFill(Color.LIGHTBLUE);
-                getChildren().add(background);
+        // 1. Fond des cellules visités
+        for (Cell[] row : maze.getGrid()) {
+            for (Cell cell : row) {
+                if (cell.visited) {
+                    Rectangle background = new Rectangle(
+                        cell.j * cellSize,
+                        cell.i * cellSize,
+                        cellSize,
+                        cellSize
+                    );
+                    background.setFill(Color.LIGHTBLUE);
+                    getChildren().add(background);
+                }
+                if (cell.isSolution) {
+                    Rectangle background = new Rectangle(
+                        cell.j * cellSize,
+                        cell.i * cellSize,
+                        cellSize,
+                        cellSize
+                    );
+                    background.setFill(Color.GREEN);
+                    getChildren().add(background);
+                }
             }
-            if (cell.isSolution) {
-                Rectangle background = new Rectangle(
-                    cell.j * cellSize,
-                    cell.i * cellSize,
-                    cellSize,
-                    cellSize
-                );
-                background.setFill(Color.GREEN);
-                getChildren().add(background);
+        }
+
+        // 2. Murs horizontaux
+        List<List<Wall>> hWalls = maze.getHWalls();
+        for (int i = 0; i < hWalls.size(); i++) {
+            for (int j = 0; j < hWalls.get(i).size(); j++) {
+                Wall wall = hWalls.get(i).get(j);
+                if (wall.isActive()) {
+                    Rectangle rect = new Rectangle(
+                        j * cellSize,
+                        (i + 1) * cellSize,
+                        cellSize,
+                        2
+                    );
+                    rect.setFill(Color.BLACK);
+                    getChildren().add(rect);
+                }
+            }
+        }
+
+        // 3. Murs verticaux
+        List<List<Wall>> vWalls = maze.getVWalls();
+        for (int i = 0; i < vWalls.size(); i++) {
+            for (int j = 0; j < vWalls.get(i).size(); j++) {
+                Wall wall = vWalls.get(i).get(j);
+                if (wall.isActive()) {
+                    Rectangle rect = new Rectangle(
+                        (j + 1) * cellSize,
+                        i * cellSize,
+                        2,
+                        cellSize
+                    );
+                    rect.setFill(Color.BLACK);
+                    getChildren().add(rect);
+                }
             }
         }
     }
-
-    // 2. Murs horizontaux
-    List<List<Wall>> hWalls = maze.getHWalls();
-    for (int i = 0; i < hWalls.size(); i++) {
-        for (int j = 0; j < hWalls.get(i).size(); j++) {
-            Wall wall = hWalls.get(i).get(j);
-            if (wall.isActive()) {
-                Rectangle rect = new Rectangle(
-                    j * cellSize,
-                    (i + 1) * cellSize,
-                    cellSize,
-                    2
-                );
-                rect.setFill(Color.BLACK);
-                getChildren().add(rect);
-            }
-        }
-    }
-
-    // 3. Murs verticaux
-    List<List<Wall>> vWalls = maze.getVWalls();
-    for (int i = 0; i < vWalls.size(); i++) {
-        for (int j = 0; j < vWalls.get(i).size(); j++) {
-            Wall wall = vWalls.get(i).get(j);
-            if (wall.isActive()) {
-                Rectangle rect = new Rectangle(
-                    (j + 1) * cellSize,
-                    i * cellSize,
-                    2,
-                    cellSize
-                );
-                rect.setFill(Color.BLACK);
-                getChildren().add(rect);
-            }
-        }
-    }
-}
 
 }
